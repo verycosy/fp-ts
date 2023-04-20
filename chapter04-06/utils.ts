7ead5a88203c72d75b4fb22bd02d359196cee2cf
Jinho Jang <verycosy@kakao.com>
2023-04-21 00:54:04 +0900

    object 유틸 함수

--- chapter04-06/utils.ts
+++ chapter04-06/utils.ts
@@ -1,11 +1,34 @@
 export function add_element_last<T>(array: T[], elem: T): T[] {
   const new_array = array.slice();
   new_array.push(elem);
-  return new_array;
+  return new_array; // or [...array, elem]
 }
 
 export function remove_items<T>(array: T[], idx: number, count: number) {
   const copy = array.slice();
   copy.splice(idx, count);
+  return copy; // or filter?
+}
+
+export function object_set<
+  T extends Record<string, unknown>,
+  K extends keyof T
+>(object: T, key: K, value: T[K]) {
+  const copy = Object.assign({}, object);
+  copy[key] = value;
   return copy;
 }
+
+export function object_delete<
+  T extends Record<string, unknown>,
+  K extends keyof T
+>(object: T, key: K): Omit<T, K> {
+  const copy = Object.assign({}, object);
+  delete copy[key];
+  return copy;
+}
+
+const item = {
+  name: 'd',
+  price: 3,
+};
