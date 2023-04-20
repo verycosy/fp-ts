5fb7516f5a981518765aee1bb4cc644a53415571
Jinho Jang <verycosy@kakao.com>
2023-04-20 23:22:40 +0900

    item 구조만 알고 있는 함수와 cart 구조만 알고 있는 함수로 나누기

--- chapter04-05/index.ts
+++ chapter04-05/index.ts
@@ -1,16 +1,23 @@
-import { BuyButton, Cart } from './type';
+import { BuyButton, Cart, ShoppingCartItem } from './type';
 
 const shopping_cart: Cart = [];
 
-function add_item(cart: Cart, name: string, price: number) {
+function make_cart_item(name: string, price: number): ShoppingCartItem {
+  return {
+    name,
+    price,
+  };
+}
+
+function add_item(cart: Cart, item: ShoppingCartItem): Cart {
   const new_cart = cart.slice();
-  new_cart.push({ name, price });
+  new_cart.push(item);
 
   return new_cart;
 }
 
-function add_item_to_cart(cart: Cart, name: string, price: number) {
-  const new_cart = add_item(cart, name, price);
+function add_item_to_cart(cart: Cart, name: string, price: number): void {
+  const new_cart = add_item(cart, make_cart_item(name, price));
 
   const amount = calc_total(new_cart);
   set_cart_total_dom(amount);
@@ -18,7 +25,7 @@ function add_item_to_cart(cart: Cart, name: string, price: number) {
   update_tax_dom(amount);
 }
 
-function calc_total(cart: Cart) {
+function calc_total(cart: Cart): number {
   let total = 0;
   for (const item of cart) {
     total += item.price;
@@ -26,7 +33,7 @@ function calc_total(cart: Cart) {
   return total;
 }
 
-function set_cart_total_dom(amount: number) {
+function set_cart_total_dom(amount: number): void {
   // TODO: DOM 업데이트
   console.log(amount);
 }
@@ -35,11 +42,11 @@ function get_buy_buttons_dom(): BuyButton[] {
   return [];
 }
 
-function isFreeShipping(cart: Cart) {
+function isFreeShipping(cart: Cart): boolean {
   return calc_total(cart) >= 20;
 }
 
-function update_shipping_icons(cart: Cart) {
+function update_shipping_icons(cart: Cart): void {
   const buttons = get_buy_buttons_dom();
 
   for (const button of buttons) {
@@ -54,16 +61,16 @@ function update_shipping_icons(cart: Cart) {
   }
 }
 
-function set_tax_dom(tax: number) {
+function set_tax_dom(tax: number): void {
   // TODO: DOM 업데이트
   console.log(tax);
 }
 
-function calc_tax(amount: number) {
+function calc_tax(amount: number): number {
   return amount * 0.1;
 }
 
-function update_tax_dom(amount: number) {
+function update_tax_dom(amount: number): void {
   set_tax_dom(calc_tax(amount));
 }
 
